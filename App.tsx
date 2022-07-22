5de9afe1e8050386e9fecc1f087cc38feba1e5ee
Jadom <matquadev@gmail.com>
2022-07-22 17:37:12 -0300

    [add] assets images

--- App.tsx
+++ App.tsx
@@ -4,12 +4,3 @@ import Home from "./src/screens/Home";
 export default function App() {
   return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -1,10 +1,23 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import React from "react";
 
 export default function Header() {
   return (
     <View>
-      <Text>Header</Text>
+      <TouchableOpacity>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/header_logo.svg")}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 100,
+    height: 50,
+    resizeMode: "contain",
+  },
+});
